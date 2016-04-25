27e06482ce5b564e69de489e68ae8de7e3b26d7d
Ganesha <reekoheek@gmail.com>
2016-04-26 03:31:45 +0700

    ADDED options on encode

--- src/JsonKit/JsonKit.php
+++ src/JsonKit/JsonKit.php
@@ -6,14 +6,14 @@ class JsonKit
 {
     protected static $stack;
 
-    public static function replaceObject($data)
+    public static function replaceObject($data, array $options = [])
     {
-        if ($data instanceof \JsonKit\JsonSerializer) {
+        if ($data instanceof JsonSerializer) {
             if (in_array($data, self::$stack)) {
                 return '*** circular ***';
             }
             array_push(self::$stack, $data);
-            $data = $data->jsonSerialize();
+            $data = $data->jsonSerialize($options);
             array_pop(self::$stack);
             if (is_object($data)) {
                 $data = (array) $data;
@@ -22,18 +22,18 @@ class JsonKit
 
         if (is_array($data)) {
             foreach ($data as $key => $value) {
-                $data[$key] = self::replaceObject($value);
+                $data[$key] = self::replaceObject($value, $options);
             }
         }
 
         return $data;
     }
 
-    public static function encode($data)
+    public static function encode($data, array $options = [])
     {
         self::$stack = array();
 
-        $jsonData = self::replaceObject($data);
+        $jsonData = self::replaceObject($data, $options);
         return json_encode($jsonData, JSON_NUMERIC_CHECK);
     }
 }
